aa969835b657bfe472d2ca8b693f4eced4863bff
Yogeshmane2611 <124765527+Yogeshmane2611@users.noreply.github.com>
2023-06-13 15:32:40 +0530

    Update signup_conn.php

--- signup_conn.php
+++ signup_conn.php
@@ -9,14 +9,41 @@ $gender=$_POST['gender'];
 $country=$_POST['country'];
 $phone_number=$_POST['phone_number'];
 
+use Aws\Ssm\SsmClient;
+use Aws\Exception\AwsException;
+
+// Create an AWS SSM client
+$ssmClient = new SsmClient([
+    'region' => 'ap-south-1',
+    'version' => 'latest',
+]);
+
+// Retrieve the IP address from Parameter Store
+try {
+    $result = $ssmClient->getParameter([
+        'Name' => '/sqldatabase/privateip',
+        'WithDecryption' => false,
+    ]);
+
+    // Access the IP address value
+    $ipAddress = $result['Parameter']['Value'];
+} catch (AwsException $e) {
+    echo $e->getMessage();
+}
+
+// Use the retrieved IP address in your PHP code
+// ...
+
+
+
 // MySQL server configuration
-$host = "10.0.1.129"; // IP address or hostname of the Linux server
+// $host = "10.0.1.129"; // IP address or hostname of the Linux server
 $username = "username";
 $password = "Admin@121";
 $dbname = "sample_website";
 
 // Connect to MySQL server
-$conn = mysqli_connect($host, $username, $password, $dbname);
+$conn = mysqli_connect($ipAddress, $username, $password, $dbname);
 
 // Check connection
 if (!$conn) {
