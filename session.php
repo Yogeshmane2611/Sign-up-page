0fb83a0f23bdadd400de60801ae45f1d6ef89605
Yogeshmane2611 <124765527+Yogeshmane2611@users.noreply.github.com>
2023-06-13 18:11:11 +0530

    Update session.php

--- session.php
+++ session.php
@@ -1,31 +1,7 @@
 
 <?php
-use Aws\Ssm\SsmClient;
-use Aws\Exception\AwsException;
 
-// Create an AWS SSM client
-$ssmClient = new SsmClient([
-    'region' => 'ap-south-1',
-    'version' => 'latest',
-]);
-
-// Retrieve the IP address from Parameter Store
-try {
-    $result = $ssmClient->getParameter([
-        'Name' => '/sqldatabase/privateip',
-        'WithDecryption' => false,
-    ]);
-
-    // Access the IP address value
-    $ipAddress = $result['Parameter']['Value'];
-} catch (AwsException $e) {
-    echo $e->getMessage();
-}
-
-// Use the retrieved IP address in your PHP code
-// ...
-
-$connection=mysqli_connect($ipAddress,"username","Admin@121");
+$connection=mysqli_connect("192.168.1.10","username","Admin@121");
 $db=mysqli_select_db($connection, "sample_website");
 session_start();
 $user_check=$_SESSION['login_user'];
