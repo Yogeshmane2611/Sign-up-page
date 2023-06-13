94b3daca0069ce3e2a618340195baf376f763760
Yogeshmane2611 <124765527+Yogeshmane2611@users.noreply.github.com>
2023-06-13 18:09:55 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -3,32 +3,8 @@ include('session.php');
 ?>
 <?php
 
-use Aws\Ssm\SsmClient;
-use Aws\Exception\AwsException;
 
-// Create an AWS SSM client
-$ssmClient = new SsmClient([
-    'region' => 'ap-south-1',
-    'version' => 'latest',
-]);
-
-// Retrieve the IP address from Parameter Store
-try {
-    $result = $ssmClient->getParameter([
-        'Name' => '/sqldatabase/privateip',
-        'WithDecryption' => false,
-    ]);
-
-    // Access the IP address value
-    $ipAddress = $result['Parameter']['Value'];
-} catch (AwsException $e) {
-    echo $e->getMessage();
-}
-
-// Use the retrieved IP address in your PHP code
-// ...
-
-$connection=mysqli_connect($ipAddress,"username","Admin@121");
+$connection=mysqli_connect("192.168.1.10","username","Admin@121");
 $db=mysqli_select_db($connection, "sample_website");
 $new_value='0';
 $online_put= mysqli_query($connection, "UPDATE Persons SET online='$new_value' WHERE email='$user_check'");
