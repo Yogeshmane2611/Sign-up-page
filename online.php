813bf4108387b0a0381d5c53bf3647b12c2f0898
Yogeshmane2611 <124765527+Yogeshmane2611@users.noreply.github.com>
2023-06-13 15:30:59 +0530

    Update online.php

--- online.php
+++ online.php
@@ -12,9 +12,33 @@
 
 
     <?php
+use Aws\Ssm\SsmClient;
+use Aws\Exception\AwsException;
+
+// Create an AWS SSM client
+$ssmClient = new SsmClient([
+    'region' => 'ap-south-1',
+    'version' => 'latest',
+]);
+
+// Retrieve the IP address from Parameter Store
+try {
+    $result = $ssmClient->getParameter([
+        'Name' => '/sqldatabase/privateip',
+        'WithDecryption' => false,
+    ]);
+
+    // Access the IP address value
+    $ipAddress = $result['Parameter']['Value'];
+} catch (AwsException $e) {
+    echo $e->getMessage();
+}
+
+// Use the retrieved IP address in your PHP code
+// ...
 
     echo '<center>'.'<h1>'."online users".'<h1>'.'<center/>';
-    $connection=mysqli_connect("10.0.1.129","username","Admin@121");
+    $connection=mysqli_connect($ipAddress,"username","Admin@121");
     $db=mysqli_select_db("sample_website",$connection);
     $online="SELECT firstname FROM Persons where online='1'";
     $record=mysqli_query($online);
